ecbfec80f9667ea0506f46efba7fc709b8c8a5f0
Ritwik Shandilya <b221046@iiit-bh.ac.in>
2024-11-13 00:57:31 +0530

    Final commit

--- JS.js
+++ JS.js
@@ -16,3 +16,15 @@ document.getElementById('eventForm').addEventListener('submit', function (e) {
   document.getElementById('errorMessage').style.display = 'none';
   displayEventDetails(eventName, eventDate, startTime, endTime, description, location);
 });
+function displayEventDetails(name, date, start, end, desc, location) {
+  const eventDetails = document.getElementById('eventDetails');
+  eventDetails.innerHTML = `
+    <p style="color: red; font-size: 24px;">You are invited!</p>
+    <h2>${name}</h2>
+    <p><strong> ${date}</strong></p>
+    <p><strong> ${start} - ${end}</strong></p>
+    <p><strong> ${location}</strong></p>
+    <p><strong> ${desc}</strong></p>
+  `;
+  eventDetails.style.display = 'block';
+}
